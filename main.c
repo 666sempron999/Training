ec656e7cb81f0cdad34e83ae8e0b8682f6dff9a1
Boris Garbuzov <boris.garbuzov@gmail.com>
2022-04-23 14:40:27 +0300

    initial 2

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "stm32f10x.h"
+// void Delay( uint32_t Val);
+
+
+while(1)
+	{
+	GPIOB->BSRR =GPIO_BSRR_BS5;		// ???????? ???.
+	Delay(800000);
+	GPIOB->BSRR =GPIO_BSRR_BS5;		// ?????????? ???. 
+	Delay(800000);
+	}
+
+}
+
+
+
+void Delay( uint32_t Val) 
+{
+  for( ; Val != 0; Val--) 
+  		{
+		__nop();
+  		}
+}
